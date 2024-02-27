b095af65cf70b31491a548a747e7702b406a07f9
Ahmed <ahmed_l2@yahoo.com>
2024-02-27 19:22:05 +0300

    .

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -12,7 +12,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	if (!node)
 		return (0);
 
-	if (node->parent && (!node->left && !node->right))
+	if (!node->left && !node->right)
 		return (1);
 
 	return (0);
