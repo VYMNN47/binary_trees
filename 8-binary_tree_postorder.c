e860df474144796ffe7494e204bc85dbed9ce3b5
Ahmed <ahmed_l2@yahoo.com>
2024-02-27 18:59:12 +0300

    .

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -13,8 +13,11 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree)
 		return;
+	
+	if (tree->left)
+		binary_tree_postorder(tree->left, func);
+	if (tree->right)
+		binary_tree_postorder(tree->right, func);
 
-	binary_tree_postorder(tree->left, func);
-	binary_tree_postorder(tree->right, func);
 	func(tree->n);
 }
