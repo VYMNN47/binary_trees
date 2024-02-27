a74a7a106892f9906e410f3fc91a24db257558c1
Ahmed <ahmed_l2@yahoo.com>
2024-02-27 19:30:35 +0300

    .

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,9 +1,24 @@
 #include "binary_trees.h"
 
+/**
+ * max - compares two ints and returns bigger int
+ * @a: first int
+ * @b: second int
+ *
+ * Return: return largests int
+ */
+
 size_t max(size_t a, size_t b)
 {
-  return (a > b) ? a : b;
+	return ((a > b) ? a : b);
 }
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: binary tree
+ *
+ * Return: returns height of binary tree
+ */
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
