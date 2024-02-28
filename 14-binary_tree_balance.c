f64a67e601ba98f9d2b3ea6c89ceec87db5c4b46
Ahmed <ahmed_l2@yahoo.com>
2024-02-28 20:13:40 +0300

    .

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,42 @@
 #include "binary_trees.h"
 
 /**
+ * max - compares two ints and returns bigger int
+ * @a: first int
+ * @b: second int
+ *
+ * Return: return largests int
+ */
+
+size_t max(size_t a, size_t b)
+{
+        return ((a > b) ? a : b);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: binary tree
+ *
+ * Return: returns height of binary tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+        size_t left = 0, right = 0;
+
+        if (!tree)
+                return (0);
+
+        if (tree->left)
+                left = binary_tree_height(tree->right) + 1;
+
+        if (tree->right)
+                right = binary_tree_height(tree->left) + 1;
+
+        return (max(left, right));
+}
+
+/**
  * binary_tree_height - measures the height of a binary tree
  * @tree: binary tree
  *
@@ -15,10 +51,10 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left)
-		left = binary_tree_balance(tree->left) + 1;
+		left = binary_tree_height(tree->left) + 1;
 
 	if (tree->right)
-		right = binary_tree_balance(tree->right) + 1;
+		right = binary_tree_height(tree->right) + 1;
 
 	return (left - right);
 }
