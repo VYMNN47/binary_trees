d54b669c7b754fd5053da400fd41bf590fbe59c9
aymnn <ayman.belahsen.4@gmail.com>
2024-02-29 23:17:29 +0100

    .

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,10 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - measures the height of a binary tree
- * @tree: pointer to the root node of the tree to measure the height of
+ * binary_tree_levelorder - traverses a binary tree using level-order traversal
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to a function to call for each node.
+ * The value in the node must be passed as a parameter to this function
  *
- * Return: the height of the tree. If tree is NULL, return 0
+ * Return: void
  */
 
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
