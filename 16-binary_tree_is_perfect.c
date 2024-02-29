317a4299119988720f04169243d28c0506897224
aymnn <ayman.belahsen.4@gmail.com>
2024-02-29 01:32:57 +0100

    .

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,15 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - measures the depth of a binary tree
- * @tree: binary tree
+ * max - compares two ints and returns bigger int
+ * @a: first int
+ * @b: second int
  *
- * Return: returns height of depth tree
+ * Return: return largests int
  */
 
 size_t max(size_t a, size_t b)
 {
-        return ((a > b) ? a : b);
+	return ((a > b) ? a : b);
 }
 
 /**
@@ -21,20 +22,26 @@ size_t max(size_t a, size_t b)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t left = 0, right = 0;
+	size_t left = 0, right = 0;
 
-        if (!tree)
-                return (0);
+	if (!tree)
+		return (0);
 
-        if (tree->left)
-                left = binary_tree_height(tree->left) + 1;
+	if (tree->left)
+		left = binary_tree_height(tree->left) + 1;
 
-        if (tree->right)
-                right = binary_tree_height(tree->right) + 1;
+	if (tree->right)
+		right = binary_tree_height(tree->right) + 1;
 
-        return (max(left, right));
+	return (max(left, right));
 }
 
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 1 if perfect, 0 otherwise. If tree is NULL, return 0
+ */
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
