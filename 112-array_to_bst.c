624a7248daa88f671ab669e9c5532af435001f5a
Ahmed <ahmed_l2@yahoo.com>
2024-03-01 02:17:58 +0300

    .

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -1,5 +1,14 @@
 #include "binary_trees.h"
 
+/**
+ * array_to_bst - Converts an array to a binary search tree
+ * @array: Pointer to the array of integers
+ * @size: Size of the array
+ *
+ * Return: Pointer to the root of the binary search tree, or NULL if the
+ *         array is NULL or empty.
+ */
+
 bst_t *array_to_bst(int *array, size_t size)
 {
 	size_t n;
