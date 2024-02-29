027ad397944ce3d62326eef807065556224ec5a3
aymnn <ayman.belahsen.4@gmail.com>
2024-02-29 01:23:17 +0100

    .

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -37,7 +37,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_height - measures the height of a binary tree
+ * binary_tree_balance - Computes the balance factor of a binary tree
  * @tree: binary tree
  *
  * Return: returns height of binary tree
