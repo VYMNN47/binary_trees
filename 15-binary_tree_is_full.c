c8c499ab99d90304427f126a8e3c84db495023dd
aymnn <ayman.belahsen.4@gmail.com>
2024-02-29 01:24:37 +0100

    .

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,20 +9,20 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-        if (!node)
-                return (0);
+	if (!node)
+		return (0);
 
-        if (!node->left && !node->right)
-                return (1);
+	if (!node->left && !node->right)
+		return (1);
 
-        return (0);
+	return (0);
 }
 
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	int left, right;
-	
+
 	if (tree == NULL)
 		return (0);
 
