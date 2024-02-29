621a5ec385a6429391519c00087487ab61d76ac5
aymnn <ayman.belahsen.4@gmail.com>
2024-02-29 01:22:03 +0100

    .

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,7 +10,7 @@
 
 size_t max(size_t a, size_t b)
 {
-        return ((a > b) ? a : b);
+	return ((a > b) ? a : b);
 }
 
 /**
@@ -22,18 +22,18 @@ size_t max(size_t a, size_t b)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t left = 0, right = 0;
+	size_t left = 0, right = 0;
 
-        if (!tree)
-                return (0);
+	if (!tree)
+		return (0);
 
-        if (tree->left)
-                left = binary_tree_height(tree->right) + 1;
+	if (tree->left)
+		left = binary_tree_height(tree->right) + 1;
 
-        if (tree->right)
-                right = binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		right = binary_tree_height(tree->left) + 1;
 
-        return (max(left, right));
+	return (max(left, right));
 }
 
 /**
